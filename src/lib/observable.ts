2977fbc1f415c0cdc90e5a65dc168cb88a3b97ca
monpro <monproshen@gmail.com>
2024-06-19 21:42:19 +1000

    feat: add pipe implementation

--- src/lib/observable.ts
+++ src/lib/observable.ts
@@ -15,4 +15,28 @@ export default class Observable<T> {
             unsubscribe: () => subscriber.unsubscribe()
         }
     }
+
+
+    /**
+     * Applies an array of operations (operators) to an observable and returns a new observable.
+     * Each operation is a function that takes an observable and returns an observable, allowing for
+     * transformations of the emitted data or handling of events like errors or completions.
+     *
+     * The `pipe` method is a mechanism for chaining multiple operators on the source observable.
+     * This chaining is essential for creating a complex sequence of data manipulation operations
+     * in a readable and manageable way.
+     *
+     * @param operations An array of operations (functions) that each transform the observable in some way.
+     * @returns An Observable that results from the sequential application of each operation on the original observable.
+     *
+     * Example usage:
+     * const processedObservable = sourceObservable.pipe(
+     *     map(data => data * 2),
+     *     filter(data => data > 10)
+     * );
+     * This example first maps (multiplies) each data item by 2, then filters out items that are 10 or less.
+     */
+    pipe(...operations: Array<(source: Observable<any>) => Observable<any>>): Observable<any> {
+        return operations.reduce<Observable<any>>((prev, op) => op(prev), this);
+    }
 }
